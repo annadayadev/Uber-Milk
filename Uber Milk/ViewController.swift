08947098754b112ac482190580eb5412898d8bae
Activelink <activelink@activelinks-macbook-air.local>
2018-11-04 16:48:41 +0800

    Initial Commit

---
+++ Uber Milk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Uber Milk
+//
+//  Created by Activelink on 04/11/2018.
+//  Copyright © 2018 Ann Adaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
